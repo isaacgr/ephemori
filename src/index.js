b3aa56bb9aa43430c50c9a5e55217806f9fe8bd0
isaac <isaacgrowell@gmail.com>
2022-06-07 23:54:14 -0400

    Disable react dev tools when in production

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+function disableReactDevTools() {
+  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== "object") {
+    return;
+  }
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    if (prop === "renderers") {
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map();
+    } else {
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] =
+        typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] === "function"
+          ? () => {}
+          : null;
+    }
+  }
+}
+
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./router/AppRouter";
@@ -6,5 +22,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./styles/index.scss";
+
+process.env.NODE_ENV === "production" && disableReactDevTools();
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
