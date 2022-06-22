64ab201f9f59e15a36d49a67977b4481434a9ba1
isaac <isaacgrowell@gmail.com>
2022-06-21 21:10:07 -0400

    Set loading to true by default for AuthProvider
    - fixes issue where components would render twice, once when auth provider
    loads with loading=false initially, and then again after it goes to false from true
    when checkAuthenticated returns

--- src/contexts/auth/AuthProvider.js
+++ src/contexts/auth/AuthProvider.js
@@ -10,7 +10,7 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const authHandler = new AuthHandler();
 
